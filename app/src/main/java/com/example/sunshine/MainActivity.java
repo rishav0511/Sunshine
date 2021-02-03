3e5a4efa17e4d03921e5113e651b3798c2d3a212
Rishav <rishavraj0511@gmail.com>
2021-02-03 21:37:42 +0530

    make json parsing and add empty textview

--- app/src/main/java/com/example/sunshine/MainActivity.java
+++ app/src/main/java/com/example/sunshine/MainActivity.java
@@ -7,11 +7,14 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.sunshine.data.SunshinePreferences;
 import com.example.sunshine.utilities.NetworkUtils;
+import com.example.sunshine.utilities.OpenWeatherJsonUtils;
 
 import java.io.IOException;
 import java.net.URL;
@@ -19,6 +22,7 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
     TextView mWeatherTextView;
     TextView mErrorMessageTextView;
+    ProgressBar mLoadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
         mErrorMessageTextView=(TextView)findViewById(R.id.error_loading_data);
+        mLoadingBar=(ProgressBar)findViewById(R.id.pb_loading_indicator);
         loadWeatherData();
     }
 
@@ -46,34 +51,54 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void loadWeatherData(){
+
+        showWeatherDataView();
         String location = SunshinePreferences.getPreferredWeatherLocation(this);
         new sunShineAsyncTask().execute(location);
     }
     public void showWeatherDataView(){
-
+        mErrorMessageTextView.setVisibility(View.INVISIBLE);
+        mWeatherTextView.setVisibility(View.VISIBLE);
     }
     public void showErrorMessage(){
-
+        mWeatherTextView.setVisibility(View.INVISIBLE);
+        mErrorMessageTextView.setVisibility(View.VISIBLE);
     }
-    public class sunShineAsyncTask extends AsyncTask<String,Void,String>
+    public class sunShineAsyncTask extends AsyncTask<String,Void,String[]>
     {
         @Override
-        protected String doInBackground(String... strings) {
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String[] doInBackground(String... strings) {
             String weatherResults=null;
             URL weatherURL = NetworkUtils.buildUrl(strings[0]);
             try {
                 weatherResults=NetworkUtils.getResponseFromHttpUrl(weatherURL);
-            } catch (IOException e) {
+                String [] simpleJsonWeatherData = OpenWeatherJsonUtils.getSimpleWeatherStringsFromJson(MainActivity.this,weatherResults);
+                return simpleJsonWeatherData;
+
+            } catch (Exception e) {
                 e.printStackTrace();
+                return null;
             }
-            return weatherResults;
         }
 
         @Override
-        protected void onPostExecute(String s) {
-            if(s!=null && !s.equals("")){
-                mWeatherTextView.setText(s);
-            }
+        protected void onPostExecute(String[] weatherData) {
+            mLoadingBar.setVisibility(View.INVISIBLE);
+           if(weatherData!=null){
+               showWeatherDataView();
+               for(String weatherstring:weatherData)
+               {
+                   mWeatherTextView.append((weatherstring)+"\n\n\n");
+               }
+           } else {
+               showErrorMessage();
+           }
         }
     }
 }
